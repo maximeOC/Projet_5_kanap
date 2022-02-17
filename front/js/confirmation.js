21bc01f943d1acabb07166c31f29738e89c8d026
Maxime GASSPARD <maxime1513@gmail.com>
2022-02-17 14:49:11 +0100

    changement order dans confirmation.js

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,10 +1,10 @@
 //afficher le numéro de commande
 
-function numberBuying() {
+function OrderNumber() {
   const numberID = new URLSearchParams(document.location.search);
   let id = numberID.get("id");
   console.log(id);
   document.getElementById("orderId").innerHTML = id;
 }
 
-numberBuying();
+OrderNumber();
